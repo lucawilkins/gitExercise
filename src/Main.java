5986b13624a03c94107c754f62d4fe1003aee6fb
lucawilkins <wilkinslt21@gcc.edu>
2025-02-17 14:22:04 -0500

    continuing the conversation

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello me");
         System.out.println("Hi how are you");
+        System.out.println("Great how are you");
     }
 }
